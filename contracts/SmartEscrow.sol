5624bc7f97f8593025cb039313c709d43276a023
warriopeaceful <ruslan777mirov@gmail.com>
2019-01-28 01:30:57 -0500

    add priceCorrectness

--- contracts/SmartEscrow.sol
+++ contracts/SmartEscrow.sol
@@ -58,21 +58,46 @@ function getAllOrdersAddress() view public returns (address[]) {
   return AllOrders;
 }
 
+// if price does not match -/+ 5% of Kyber return false
+function priceCorrectness(address _orderAddress)
+view
+public
+returns (bool) {
+  var order = orders[_orderAddress];
+
+  uint256 curentPrice = getValue(order.TokenA, order.TokenB, order.amount);
+  uint256 fivePercent = order.price.div(100).mul(5);
+  uint256 maxPrice = order.price.add(fivePercent);
+  uint256 minPrice = order.price.sub(fivePercent);
+
+  if(curentPrice > maxPrice || curentPrice < minPrice){
+    return false;
+  }else{
+    return true;
+  }
+}
+
 // Not finished
 // Need approve before execude
 // TODO require compare input and order.price -/+ 5% of Kyber
 function execudeOrder(address _tokenA, address _tokenB, uint256 _value, address _orderAddress) public {
+
+  // need price match -/+ 5% of Kyber
+  require(priceCorrectness(_orderAddress));
 
   ERC20 tokenA = ERC20(_tokenA);
   ERC20 tokenB = ERC20(_tokenB);
 
   var order = orders[_orderAddress];
 
-  require(tokenB.transferFrom(msg.sender, address(this), _value));
+  // Get value A in B
+  uint256 A_value = getValue(_tokenA, _tokenB, _value);
+
+  require(tokenB.transferFrom(msg.sender, address(this), A_value));
 
   require(tokenA.transfer(msg.sender, _value));
 
-  require(tokenB.transfer(_orderAddress, _value));
+  require(tokenB.transfer(_orderAddress, A_value));
 
   order.amount = order.amount.sub(_value);
 }
